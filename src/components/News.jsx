9c6787288e2c8814d8079323d14b08162ec14a3d
Raheem Mubarak <raheemmubarak7@gmail.com>
2022-08-12 20:42:41 +0100

    Update

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,3 +1,13 @@
 import React, { useState } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
+
+import { useGetCryptosQuery } from "../services/CryptoApi";
+import { useGetCryptoNewsQuery } from "../services/CryptoNewsApi";
+import Loader from "./Loader";
+
+const demoImage =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
