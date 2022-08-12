47ea549635d218d65cb4d1da95e6ec58a77ab535
Raheem Mubarak <raheemmubarak7@gmail.com>
2022-08-12 20:45:39 +0100

    Update

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -43,3 +43,17 @@ const News = ({ simplified }) => {
           </Select>
         </Col>
       )}
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  style={{ maxWidth: "200px", maxHeight: "100px" }}
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt=""
+                />
+              </div>
